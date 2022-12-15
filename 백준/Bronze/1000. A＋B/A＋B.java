0b406a6e3a1aff64bccce03693dd1c3e917261ef
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2022-12-15 19:31:59 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int A, B;
+		Scanner sc = new Scanner(System.in);
+		
+		A=sc.nextInt();
+		B=sc.nextInt();
+		
+		System.out.println(A+B);
+
+	}
+
+}
