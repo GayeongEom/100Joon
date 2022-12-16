d93f3b19e3926d0c52cbd5006402bebb4f86ba2e
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2022-12-16 18:41:19 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		int A, B ;
+		Scanner sc= new Scanner(System.in);
+		
+		A=sc.nextInt();
+		B=sc.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+
+	}
+
+}
