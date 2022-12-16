7a48e43a45ee49fb87ad1cd916575d813af9fab4
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2022-12-16 19:15:55 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int y;
+		Scanner sc=new Scanner(System.in);
+		y=sc.nextInt() - 543;
+		System.out.println(y);
+
+	}
+
+}
