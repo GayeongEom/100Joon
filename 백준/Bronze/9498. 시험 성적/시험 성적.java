9e07ead12fa148dc667300907e6792f6118be4fc
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2022-12-31 19:15:53 +0900

    [Bronze V] Title: 시험 성적, Time: 220 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int grade = scanner.nextInt();
+		
+		if(grade >= 90) {
+			System.out.println("A");
+		} else if(grade >= 80) {
+			System.out.println("B");
+		} else if(grade >= 70) {
+			System.out.println("C");
+		} else if(grade >= 60) {
+			System.out.println("D");
+		} else System.out.println("F");
+	
+	} //main
+
+} //class
