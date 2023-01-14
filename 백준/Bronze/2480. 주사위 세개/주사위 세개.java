1d1da5747ff01a31e25b036f05c2ed625512eb94
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-01-14 22:31:42 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -11,22 +11,21 @@ public class Main {
 		
 		int prize = 0;
 		
-		//3개가 다 같은건 1번
-		//2개가 같은건 3번
-		//모두 다른게 가장 많음
-		if(x==y) { //2개가 같은 상황
-			if(x==z) { //3개가 같음
-				prize = 10000 + (x * 1000);
+		if(x==y && y==z) { //3개 같을 때
+			prize = 10000 + (x*1000);
+		
+		} else if(x==y || x==z || y==z) { //2개가 같을 때
+			if(x==y || x==z) {
+				prize = 1000 +(x*100);
 			} else {
-				prize = 1000 + (x * 100);
-			}
-		} else if(x==z) { //2개가 같은 상황
-			prize = 1000 + (x * 100);
-		} else if(y==z ) { //2개가 같은 상황
-			prize = 1000 + (z * 100);
+				prize = 1000 +(z*100);
+			}	
+		
 		} else { //모두 다를 때
 			prize = Math.max(x, Math.max(y, z)) * 100;
 		}
+		
+		
 		
 		//출력
 		System.out.println(prize);		
