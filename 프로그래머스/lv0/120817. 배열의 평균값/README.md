1fcb4191c76833d29a16c554abc1f29c0bf366ac
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-01-15 23:28:50 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/README.md
+++ 프로그래머스/lv0/120817. 배열의 평균값/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 평균값 - 120817 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120817) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120817?language=java) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.05 ms
+메모리: 75.9 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,7 +1,10 @@
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        answer = (numbers[0] + numbers[numbers.length-1])/(double)2;
-        return answer;
+        
+        for(int i=0; i<numbers.length; i++) {
+            answer += numbers[i];
+        }
+        return answer/numbers.length;
     }
 }
