0df3508d633a002d44276e4934b8beb83a38fbd1
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-01-29 22:04:46 +0900

    [Bronze V] Title: A+B - 4, Time: 88 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		//입출력 객체
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		
+//		while(true) {
+//			
+//			
+//			String tc = br.readLine();
+//			
+//			if(tc == null) {
+//				
+//				break;
+//				
+//			}
+//			
+//			
+//			int a = Integer.parseInt(tc.split(" ")[0]);
+//			int b = Integer.parseInt(tc.split(" ")[1]);
+//			
+//			
+//			bw.write((a+b) + "\n");
+//			
+//		}
+//		
+//		bw.flush();
+		
+		
+		
+		
+		 String input;
+		 
+		    while ((input = br.readLine()) != null) {
+		    	
+		        String[] nums = input.split(" ");
+		        
+		        System.out.println(Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]));
+		   
+		    }
+		
+		
+		
+		
+	}
+
+}
