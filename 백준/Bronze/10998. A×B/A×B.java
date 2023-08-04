ca684c9b1405ef43f4e11be7b64ac7bc07d87868
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-04 16:38:11 +0900

    [Bronze V] Title: A×B, Time: 84 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,19 +1,18 @@
-
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int A,B;
-	static int answer;
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		A =sc.nextInt();
-		B =sc.nextInt();
-		
-		System.out.println(A*B);
-		
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(a*b);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
