3e400def8fa7aaed042368cf5548be4a3313dce2
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-04 17:47:20 +0900

    [Bronze III] Title: 곱셈, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,16 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		String B = br.readLine();
+		char[] b = B.toCharArray();
 		
-		System.out.println((B%10)*A);
-		System.out.println((B/10)%10*A);
-		System.out.println((B/100)*A);
-		System.out.println(A*B);
+		System.out.println(A*(b[2]-'0'));
+		System.out.println(A*(b[1]-'0'));
+		System.out.println(A*(b[0]-'0'));
+		System.out.println(A*Integer.parseInt(B));
 	}
-
 }
