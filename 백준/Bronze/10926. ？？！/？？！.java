ac1fe49a8620fd4c2ff28ae32c979e5ebee2cf81
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-04 16:55:51 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,15 +1,15 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
 	public static void main(String[] args) {
-		Scanner scanner =new Scanner(System.in);
-		String id;
-		id = scanner.nextLine();
-		System.out.println(id+"??!");
-		
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println(br.readLine()+"??!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
