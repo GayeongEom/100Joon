cadf21287c7504b8f9a05170f976a3ff9bb40038
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-07 15:02:52 +0900

    [Bronze IV] Title: 주사위 세개, Time: 76 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -10,18 +10,15 @@ public class Main {
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
 		int C = Integer.parseInt(st.nextToken());
+
+		//조건 정리
 		if(A==B && B==C) {
 			System.out.println(10000+(A*1000));
-		} else if(A!=B && B!=C && A!=C) {
-			int L = Math.max(A, B);
-			L = Math.max(L, C);
-			System.out.println(L*100);
+		} else if(A==B || B==C || A==C) {
+			if(A==B || A==C) System.out.println(1000+(A*100));
+			else System.out.println(1000+B*100);
 		} else {
-			int S=0;
-			if(A==B) S=A;
-			else if(B==C) S=B;
-			else S=C;
-			System.out.println(1000+(S*100));
+			System.out.println(Math.max(A, Math.max(B, C))*100);
 		}
 	}
 }
