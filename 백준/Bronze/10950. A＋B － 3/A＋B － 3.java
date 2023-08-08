60bc3902d97db26979c5208f8eafaaaddd349b03
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-08 14:23:01 +0900

    [Bronze V] Title: A+B - 3, Time: 92 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int test = sc.nextInt();
-		for (int i = 1; i <= test; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println(a + b);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(bf.readLine());
+		for(int i=0;i<T;i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			System.out.println(A+B);
 		}
 	}
 
