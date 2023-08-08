1afbc0b18fe8c8e048c106b82411ef0e2fab8172
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-08 16:26:13 +0900

    [Bronze V] Title: A+B - 4, Time: 80 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,45 +1,54 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-
-		//입출력 객체
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String input = "";
 		
+//		while((input=br.readLine())!=null && !input.isEmpty()) {
+//			st = new StringTokenizer(br.readLine());
+//			int A = Integer.parseInt(st.nextToken());
+//			int B = Integer.parseInt(st.nextToken());
+//			sb.append(A+B).append("\n");
+//		}
+//		System.out.println(sb);
+		
+		//true로 while문 돌리기
+//		while(true) {
+//			if((input=br.readLine())==null) break;
+//			st = new StringTokenizer(br.readLine());
+//			int A = Integer.parseInt(st.nextToken());
+//			int B = Integer.parseInt(st.nextToken());
+//			sb.append(A+B).append("\n");
+//		}
+//		System.out.println(sb);
+		
+		//다른 사람 코드
+//		while((input=br.readLine())!=null) {
+//			st = new StringTokenizer(input);
+//			int A = Integer.parseInt(st.nextToken());
+//			int B = Integer.parseInt(st.nextToken());
+//			sb.append(A+B).append("\n");
+//		}
+//		System.out.println(sb);
+
+		//if문 break버전
 		while(true) {
-			
-			
-			String tc = br.readLine();
-			
-			if(tc == null) {
-				
-				break;
-				
-			}
-			
-			
-			int a = Integer.parseInt(tc.split(" ")[0]);
-			int b = Integer.parseInt(tc.split(" ")[1]);
-			
-			
-			bw.write((a+b) + "\n");
-			
-			bw.flush();
+			if((input=br.readLine())==null) break;
+			st = new StringTokenizer(input);
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sb.append(A+B).append("\n");
 		}
-		
-		
-
-		
-		
-		
-		
+		System.out.println(sb);
 	}
 
 }
+
+//br eof 처리하는 방법!
+//true로 while문을 돌리고 if문으로 break 걸어주기...
