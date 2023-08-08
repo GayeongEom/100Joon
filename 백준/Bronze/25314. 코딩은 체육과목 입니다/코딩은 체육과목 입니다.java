a45fd003cc37edcf46ff276e82c3eb8a7cac58bf
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-08 15:24:48 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 72 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<(N/4); i++) {
+			sb.append("long ");
+		}
+		sb.append("int");
+		System.out.println(sb);
+	}
+}
