1c20834807693afa35ad5f14e5f1c37f2f7c317d
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-08 14:14:48 +0900

    [Bronze V] Title: 구구단, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		//입력
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		for(int i=1; i<=9; i++) {
-			System.out.printf("%d * %d = %d\n", n, i, n*i);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(bf.readLine());
+		for(int i=1;i<10;i++) {
+			System.out.println(A+" * "+i+" = "+(A*i));
 		}
-		
 	}
 
 }
