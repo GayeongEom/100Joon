33d60d3197fd18ffdec42bfecbabba9366ff060c
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-08 15:37:05 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 712 ms, Memory: 241032 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,37 +1,20 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		try {
-			int n = Integer.parseInt(br.readLine());
-			
-			for(int i=0; i<n; i++) {
-				
-				String s = br.readLine();
-				
-				int a = Integer.parseInt(s.split(" ")[0]);
-				int b = Integer.parseInt(s.split(" ")[1]);
-				
-				bw.write(a + b + "\n");
-			
-			}
-			
-			bw.flush();
-			
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<T; i++) {
+			st = new StringTokenizer(br.readLine());
+			sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()));
+			sb.append("\n");
 		}
-		
+		System.out.println(sb);
 	}
 
 }
