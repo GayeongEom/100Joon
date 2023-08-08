92da52dc8af60a40820f8d77dfbcf1bbdb8f3a48
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-08 16:39:04 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,41 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		//입출력 객체
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+
+//		for(int i=1; i<=N; i++) {
+//			for(int j=0;j<i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 		
-		//N개 읽기
-		try {
-			
-			int N = Integer.parseInt(br.readLine());
-			
-			for(int i=0; i<N; i++) {
-				for (int j=0; j<i+1; j++) {
-					
-					bw.write("*");
-					
-				}//for j
-				
-				bw.write("\n");
-				
-			}//for i
-			
-			bw.flush();
-			
-			
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		//sb 버전
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=N; i++) {
+			for(int j=0; j<i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
 		}
-		
+		System.out.println(sb);
 	}
-	
+
 }
+
+//더 간단한 방법이 있을 것 같은디..
