c8ee93a419ec368f6c65470aa21927c8df101b01
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-08-19 22:14:51 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -1,20 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] d, int budget) {
-        int answer = 0;
+  public int solution(int[] d, int budget) {
+      int answer = 0;
+
         Arrays.sort(d);
-        
-        for(int i=0; i<d.length; i++) {
+
+        for (int i = 0; i < d.length; i++) {
             budget -= d[i];
-            if(budget<0) {
-                answer = i;
-                break;
-            } else {
-                answer = d.length;
-            }
+
+            if (budget < 0) break;
+
+            answer++;
         }
-        
+
         return answer;
-    }
+  }
 }
