c528d7ddaf1b39e04bb06251a94e0fabdbf9e34d
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-09-05 22:26:47 +0900

    [unrated] Title: 과일 장수, Time: 0.42 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -3,18 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        ArrayList<Integer> arr = new ArrayList<>();
         
-        if(score.length<m) return answer;
-        for(int i : score) {
-            arr.add(i);
-        }
-        Collections.sort(arr, Collections.reverseOrder());
-        int iter = score.length/m;
-        int idx = m-1;
-        for(int i=0; i<iter; i++) {
-            answer += arr.get(idx) * m;
-            idx += m;
+        Arrays.sort(score);
+        for(int i=score.length; i >=m; i-=m) {
+            answer += score[i-m] * m;
         }
         
         
