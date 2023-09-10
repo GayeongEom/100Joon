6d96f361e88aae5993a06ec5fa74f18c8c5aa8e2
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-09-10 14:54:10 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,12 +1,8 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length-n+1];
-        int idx = 0;
-        
-        for(int i=n-1; i<num_list.length; i++) {
-            answer[idx++] = num_list[i];
-        }
-        
+        int[] answer = Arrays.copyOfRange(num_list, n-1, num_list.length);
         return answer;
     }
 }
