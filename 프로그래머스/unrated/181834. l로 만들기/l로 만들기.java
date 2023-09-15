ea808b8a1d434b6e91d639008ebebdbc9472fc44
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-09-15 23:10:45 +0900

    [unrated] Title: l로 만들기, Time: 19.63 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        
-        for(char c : myString.toCharArray()) {
-            answer += (c<'l') ? "l" : c;
-        }
-        
-        return answer;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
