f0fb79e5cf8da7dfb8f5beb5c11c5fe30261eefc
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-09-16 23:09:20 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.31 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.trim().split("\\s+");
+        
+        
+        
+        return answer;
+    }
+}
