9d0d3b98efe90d648dd2a0dd9378e3da0b5da648
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-09-17 21:56:30 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.33 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,14 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[5];
+        int[] answer = {};
         
         Arrays.sort(num_list);
         
-        for(int i=0; i<5; i++) {
-            answer[i] = num_list[i];
-        }
         
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
