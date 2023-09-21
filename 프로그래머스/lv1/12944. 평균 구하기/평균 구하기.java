87ab290590732d6c64500dee4a3b5aff2fd1c886
Gayeong <120368353+GayeongEom@users.noreply.github.com>
2023-09-21 21:12:56 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -1,13 +1,14 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-        int sum = 0;
+        double sum = 0;
         
-        for(int i=0; i<arr.length; i++) {
-            sum += arr[i];
+        for(int i : arr) {
+            sum += i;
         }
         
-        answer = (double)sum/arr.length;
+        answer = sum/arr.length;
+        
         
         return answer;
     }
